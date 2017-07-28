c0a90610d6450f02521eeeed568d2cb0089680fa
Jaume Bosch <jbosch@bsc.es>
2017-07-28 16:47:28 +0200

    Update version system

---
+++ src/libxtasks_version.h
@@ -0,0 +1,9 @@
+#ifndef __LIBXTASKS_VERSION_H__
+#define __LIBXTASKS_VERSION_H__
+
+#define LIBXTASKS_VER_MAJOR 0
+#define LIBXTASKS_VER_MINOR 1
+
+//This comment should be replaced by the commit build info after the "make install" step
+
+#endif
