b8cbaa243f014455053a460b7af2a895984ede98
Jaume Bosch <jbosch@bsc.es>
2017-08-04 08:14:37 +0200

    Bump version to 1.2

--- src/libxtasks_version_template.h
+++ src/libxtasks_version_template.h
@@ -2,7 +2,7 @@
 #define __LIBXTASKS_VERSION_H__
 
 #define LIBXTASKS_VERSION_MAJOR 1
-#define LIBXTASKS_VERSION_MINOR 1
+#define LIBXTASKS_VERSION_MINOR 2
 
 //This comment should be replaced by the commit build info after the "make install" step
 
