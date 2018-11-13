2faffeaa8919c5ebbf91219754c86a582c965686
Antonio Filgueras <antonio.filgueras@bsc.es>
2018-11-13 17:46:00 +0100

    Bump version

--- src/libxtasks_version_template.h
+++ src/libxtasks_version_template.h
@@ -1,7 +1,7 @@
 #ifndef __LIBXTASKS_VERSION_H__
 #define __LIBXTASKS_VERSION_H__
 
-#define LIBXTASKS_VERSION_MAJOR 5
+#define LIBXTASKS_VERSION_MAJOR 6
 #define LIBXTASKS_VERSION_MINOR 0
 
 //This comment should be replaced by the commit build info after the "make install" step
