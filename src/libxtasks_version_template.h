d9d4e6a712e049ca44ccdd2ad1abe7cc5d37b4fe
Jaume Bosch <jbosch@bsc.es>
2019-09-16 18:16:38 +0200

    Bump version to 7.3

--- src/libxtasks_version_template.h
+++ src/libxtasks_version_template.h
@@ -22,7 +22,7 @@
 #define __LIBXTASKS_VERSION_H__
 
 #define LIBXTASKS_VERSION_MAJOR 7
-#define LIBXTASKS_VERSION_MINOR 2
+#define LIBXTASKS_VERSION_MINOR 3
 
 //This comment should be replaced by the commit build info after the "make install" step
 
