012649753b4770fed2cc27080f954f688dc3068a
Jaume Bosch <jbosch@bsc.es>
2021-03-24 12:56:00 +0100

    Bump version to 10.1

--- src/libxtasks_version_template.h
+++ src/libxtasks_version_template.h
@@ -22,7 +22,7 @@
 #define __LIBXTASKS_VERSION_H__
 
 #define LIBXTASKS_VERSION_MAJOR 10
-#define LIBXTASKS_VERSION_MINOR 0
+#define LIBXTASKS_VERSION_MINOR 1
 
 // This comment should be replaced by the commit build info after the "make install" step
 
