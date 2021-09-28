0d92a593f6f3b35ad31c8e239899e09c88c08ad7
Antonio Filgueras <antonio.filgueras@bsc.es>
2021-09-28 10:07:40 +0000

    Bump version to 1.12

--- src/libxtasks_version_template.h
+++ src/libxtasks_version_template.h
@@ -21,7 +21,7 @@
 #ifndef __LIBXTASKS_VERSION_H__
 #define __LIBXTASKS_VERSION_H__
 
-#define LIBXTASKS_VERSION_MAJOR 11
+#define LIBXTASKS_VERSION_MAJOR 12
 #define LIBXTASKS_VERSION_MINOR 1
 
 // This comment should be replaced by the commit build info after the "make install" step
