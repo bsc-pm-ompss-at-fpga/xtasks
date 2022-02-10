a035193a3f0ad564f05c3bc69e2108486e5218f4
Miquel Vidal Piñol <miquel.vidal@bsc.es>
2022-02-10 15:39:33 +0100

    Bump version to 13.0

--- src/libxtasks_version_template.h
+++ src/libxtasks_version_template.h
@@ -21,8 +21,8 @@
 #ifndef __LIBXTASKS_VERSION_H__
 #define __LIBXTASKS_VERSION_H__
 
-#define LIBXTASKS_VERSION_MAJOR 12
-#define LIBXTASKS_VERSION_MINOR 1
+#define LIBXTASKS_VERSION_MAJOR 13
+#define LIBXTASKS_VERSION_MINOR 0
 
 // This comment should be replaced by the commit build info after the "make install" step
 
