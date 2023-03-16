f67dc702cc054e678f8dceab5b3d78fd79f720f5
Juan Miguel de Haro <juan.deharoruiz@bsc.es>
2023-03-16 10:51:39 +0100

    Bump version to 15.0

--- src/libxtasks_version_template.h
+++ src/libxtasks_version_template.h
@@ -21,8 +21,8 @@
 #ifndef __LIBXTASKS_VERSION_H__
 #define __LIBXTASKS_VERSION_H__
 
-#define LIBXTASKS_VERSION_MAJOR 14
-#define LIBXTASKS_VERSION_MINOR 3
+#define LIBXTASKS_VERSION_MAJOR 15
+#define LIBXTASKS_VERSION_MINOR 0
 
 // This comment should be replaced by the commit build info after the "make install" step
 
