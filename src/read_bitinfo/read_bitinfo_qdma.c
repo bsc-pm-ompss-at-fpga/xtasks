7eba7779eead7c39195d6c945f329af0368c8a78
Miquel Vidal i Pinyol <miquel.vidal@bsc.es>
2024-01-30 12:33:47 +0100

    Adapted read_bitinfo for multi-FPGA environments

--- src/read_bitinfo/read_bitinfo_qdma.c
+++ src/read_bitinfo/read_bitinfo_qdma.c
@@ -10,56 +10,87 @@
 #define BITINFO_ADDRESS 0x0
 #define BITINFO_MAX_SIZE 4096
 #define PCI_DEV_BASE_PATH "/sys/bus/pci/devices"
-#define PCI_DEV "0000:02:00.0"
 #define PCI_BAR_FILE "resource2"
 #define XTASKS_PCIDEV_ENV "XTASKS_PCI_DEV"
 #define PCI_MAX_PATH_LEN 64
-#define PCI_BAR_SIZE 0x40000  // map only 256KB even if the BAR is larger
+#define PCI_BAR_SIZE 0x200000  // map 2MB
 
-static uint32_t *bitinfo;
+#define MAX_DEVICES 16
+
+static int _ndevs = 0;
+static char *_pciDevNames[MAX_DEVICES];
+static uint32_t *bitinfo[MAX_DEVICES];
 
 int read_bitinfo(const uint32_t *bitinfo);
 
-const char *getPciDevName()
+static const char *getPciDevList()
 {
-    const char *dev = getenv(XTASKS_PCIDEV_ENV);
-    return dev ? dev : PCI_DEV;
+    const char *devList = getenv(XTASKS_PCIDEV_ENV);
+    if (devList == NULL) {
+        fprintf(stderr,
+            "Environment variable XTASKS_PCIDEV_ENV"
+            " not found, set it to the pci device ID list,"
+            " in the form of xxxx:xx:xx.x see `lspci -Dd 10ee:`\n");
+    }
+    return devList;
 }
 
 int init()
 {
+    const char *pciDevListEnv = getPciDevList();
+    if (pciDevListEnv == NULL) {
+        goto init_devlist_err;
+    }
+
     // Map PCI BAR into user space
-    char pciBarPath[PCI_MAX_PATH_LEN];
-    const char *pciDevname = getPciDevName();
-    volatile uint32_t *_pciBar;
-    int _pciBarFd;
-
-    snprintf(pciBarPath, PCI_MAX_PATH_LEN, "%s/%s/%s", PCI_DEV_BASE_PATH, pciDevname, PCI_BAR_FILE);
-    _pciBarFd = open(pciBarPath, O_RDWR);
-    if (_pciBarFd < 0) {
-        perror("Could not open PCIe register window");
-        if (errno == ENOENT) {
-            fprintf(stderr, "Note: Set " XTASKS_PCIDEV_ENV
-                            " to the pci device ID, in the form of\
-                    xxxx:xx:xx.x see `lspci -Dd 10ee:`");
+    int ndevs = 0;
+    _ndevs = 0;
+    char *pciDevName;
+    char *pciDevList = malloc(strlen(pciDevListEnv) + 1);
+    strcpy(pciDevList, pciDevListEnv);
+    pciDevName = strtok(pciDevList, " ");
+    while (pciDevName != NULL) {
+        if (ndevs == MAX_DEVICES) {
+            fprintf(stderr, "Found too many devices\n");
+            goto init_maxdev_err;
         }
-        goto init_pcibar_open_err;
-    }
-    _pciBar = mmap(NULL, PCI_BAR_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, _pciBarFd, 0);
-    close(_pciBarFd);
-    if (_pciBar == MAP_FAILED) {
-        perror("Could not map BAR into process memory space");
-        goto init_pcibar_mmap_err;
-    }
+        uint32_t *pciBar;
+        char pciBarPath[PCI_MAX_PATH_LEN];
+        snprintf(pciBarPath, PCI_MAX_PATH_LEN, "%s/%s/%s", PCI_DEV_BASE_PATH, pciDevName, PCI_BAR_FILE);
+        int pciBarFd = open(pciBarPath, O_RDWR);
+        if (pciBarFd < 0) {
+            perror("Could not open PCIe register window");
+            if (errno == ENOENT) {
+                fprintf(stderr, "Cound not open %s\n", pciBarPath);
+            }
+            goto init_open_bar_err;
+        }
+        pciBar = mmap(NULL, PCI_BAR_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, pciBarFd, 0);
+        close(pciBarFd);
+        if (pciBar == MAP_FAILED) {
+            fprintf(stderr, "Could not map BAR into process memory space\n");
+            goto init_map_bar_err;
+        }
 
-    bitinfo = malloc(BITINFO_MAX_SIZE);
-    memcpy(bitinfo, (void *)(_pciBar + BITINFO_ADDRESS / sizeof(*_pciBar)), BITINFO_MAX_SIZE);
-    munmap((void *)_pciBar, PCI_BAR_SIZE);
+        fprintf(stderr, "Found device %s\n", pciDevName);
+
+        bitinfo[ndevs] = malloc(BITINFO_MAX_SIZE);
+        memcpy(bitinfo[ndevs], (void *)(pciBar + BITINFO_ADDRESS / sizeof(*pciBar)), BITINFO_MAX_SIZE);
+        munmap((void *)pciBar, PCI_BAR_SIZE);
+
+        _pciDevNames[ndevs] = strdup(pciDevName);
+
+        ++ndevs;
+        pciDevName = strtok(NULL, " ");
+    }
+    _ndevs = ndevs;
 
     return 0;
 
-init_pcibar_open_err:
-init_pcibar_mmap_err:
+init_devlist_err:
+init_maxdev_err:
+init_open_bar_err:
+init_map_bar_err:
     return 1;
 }
 
@@ -69,9 +100,13 @@ int main()
         return 1;
     }
 
-    int ret = read_bitinfo(bitinfo);
+    unsigned ndev;
+    for (ndev = 0; ndev < _ndevs; ndev++) {
+        printf("Reading bitinfo of FPGA %s\n", _pciDevNames[ndev]);
+        read_bitinfo(bitinfo[ndev]);
+        free(bitinfo[ndev]);
+        free(_pciDevNames[ndev]);
+    }
 
-    free(bitinfo);
-
-    return ret;
+    return 0;
 }
