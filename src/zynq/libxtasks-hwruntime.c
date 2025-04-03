c4ad50ae9659e610602e48a54d0fd17284692845
Juan Miguel de Haro <juan.deharoruiz@bsc.es>
2025-04-03 08:43:49 +0200

    Add defines for to enable/disable instrumentation buffer invalidation

--- src/zynq/libxtasks-hwruntime.c
+++ src/zynq/libxtasks-hwruntime.c
@@ -736,7 +736,7 @@ xtasks_stat xtasksGetInstrumentData(xtasks_acc_handle const accel, xtasks_ins_ev
         return XTASKS_ENOAV;
 
     count = min(maxCount, _numInstrEvents - acc->instrIdx);
-    validEvents = getAccEvents(acc, events, count, _numInstrEvents, _instrBuffHandle, 1);
+    validEvents = getAccEvents(acc, events, count, _numInstrEvents, _instrBuffHandle, INSTR_BUF_INVALIDATE);
     if (validEvents < 0) {
         return XTASKS_ERROR;
     }
