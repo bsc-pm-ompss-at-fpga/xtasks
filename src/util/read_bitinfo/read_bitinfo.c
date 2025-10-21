ae1ad9e6f5fbbb2bcae084d1f04d6069a3b4c73c
Miquel Vidal i Pinyol <miquel.vidal@bsc.es>
2025-10-21 12:22:25 +0200

    Update read_bitinfo text

--- src/util/read_bitinfo/read_bitinfo.c
+++ src/util/read_bitinfo/read_bitinfo.c
@@ -13,56 +13,62 @@ int read_bitinfo(const uint32_t* bitinfo) {
                 BITINFO_MIN_REV);
         return 1;
     }
-    printf("Bitinfo version:\t%d\n", bitinfoRev);
-    printf("Bitstream user-id:\t0x%X\n", bitinfo_get_bitstream_userid(bitinfo));
+    printf("Bitinfo version: %d\n", bitinfoRev);
+    printf("Bitstream user-id: 0x%X\n", bitinfo_get_bitstream_userid(bitinfo));
     int major, minor, patch;
     bitinfo_get_AIT_version(bitinfo, &major, &minor, &patch);
-    printf("AIT version:\t%u.%u.%u\n", major, minor, patch);
-    printf("Wrapper version\t%u\n", bitinfo_get_wrapper_version(bitinfo));
-    printf("Number of acc:\t%d\n", bitinfo_get_acc_count(bitinfo));
-    printf("Board base frequency (MHz)\t%f\n", (float)bitinfo_get_board_base_freq(bitinfo) / 1000000);
-    printf("Interleaving ");
-    bitinfo_get_interleaving_stride(bitinfo) ? printf("stride\t%u\n", bitinfo_get_interleaving_stride(bitinfo))
+    printf("AIT version: %u.%u.%u\n", major, minor, patch);
+    printf("Wrapper version: %u\n", bitinfo_get_wrapper_version(bitinfo));
+    printf("Number of accelerators: %d\n", bitinfo_get_acc_count(bitinfo));
+    printf("Board base frequency: %.2f MHz\n", (float)bitinfo_get_board_base_freq(bitinfo) / 1000000);
+    printf("Dedicated FPGA memory: ");
+    bitinfo_get_memory_size(bitinfo) ? printf("%" PRIu32 " GB\n", bitinfo_get_memory_size(bitinfo))
+                                     : printf("not available\n");
+    printf("Memory interleaving: ");
+    bitinfo_get_interleaving_stride(bitinfo) ? printf("%u\n", bitinfo_get_interleaving_stride(bitinfo))
                                              : printf("not enabled\n");
     printf("\nFeatures:\n");
     printf("[%c] Instrumentation\n", bitinfo_get_feature(bitinfo, BIT_FEATURE_INST) ? 'x' : ' ');
     printf("[%c] Hardware counter\n", bitinfo_get_feature(bitinfo, BIT_FEATURE_HWCOUNTER) ? 'x' : ' ');
-    printf("[%c] Performance interconnect\n", bitinfo_get_feature(bitinfo, BIT_FEATURE_INTERCONNECT_OPT) ? 'x' : ' ');
-    printf("[%c] Simplified interconnection\n",
-           bitinfo_get_feature(bitinfo, BIT_FEATURE_INTERCONNECT_SIMPL) ? 'x' : ' ');
-    printf("[%c] POM AXI-Lite\n", bitinfo_get_feature(bitinfo, BIT_FEATURE_POM_AXILITE) ? 'x' : ' ');
-    printf("[%c] POM task creation\n", bitinfo_get_feature(bitinfo, BIT_FEATURE_POM_TASK_CREATE) ? 'x' : ' ');
-    printf("[%c] POM dependencies\n", bitinfo_get_feature(bitinfo, BIT_FEATURE_POM_DEPS) ? 'x' : ' ');
-    printf("[%c] POM lock\n", bitinfo_get_feature(bitinfo, BIT_FEATURE_POM_LOCK) ? 'x' : ' ');
-    printf("[%c] POM spawn queues\n", bitinfo_get_feature(bitinfo, BIT_FEATURE_SPAWN_Q) ? 'x' : ' ');
+    printf("Interconnect optimization\n");
+    printf("\t[%c] Area\n", bitinfo_get_feature(bitinfo, BIT_FEATURE_INTERCONNECT_OPT) ? ' ' : 'x');
+    printf("\t[%c] Performance\n", bitinfo_get_feature(bitinfo, BIT_FEATURE_INTERCONNECT_OPT) ? 'x' : ' ');
+    printf("Picos OmpSs Manager\n");
+    printf("\t[%c] AXI-Lite\n", bitinfo_get_feature(bitinfo, BIT_FEATURE_POM_AXILITE) ? 'x' : ' ');
+    printf("\t[%c] Task creation\n", bitinfo_get_feature(bitinfo, BIT_FEATURE_POM_TASK_CREATE) ? 'x' : ' ');
+    printf("\t[%c] Dependencies\n", bitinfo_get_feature(bitinfo, BIT_FEATURE_POM_DEPS) ? 'x' : ' ');
+    printf("\t[%c] Lock\n", bitinfo_get_feature(bitinfo, BIT_FEATURE_POM_LOCK) ? 'x' : ' ');
+    printf("\t[%c] Spawn queues\n", bitinfo_get_feature(bitinfo, BIT_FEATURE_SPAWN_Q) ? 'x' : ' ');
     printf("[%c] Power monitor (CMS)\n", bitinfo_get_feature(bitinfo, BIT_FEATURE_CMS_EN) ? 'x' : ' ');
     printf("[%c] Thermal monitor (sysmon)\n", bitinfo_get_feature(bitinfo, BIT_FEATURE_SYSMON_EN) ? 'x' : ' ');
     printf("[%c] OMPIF\n", bitinfo_get_feature(bitinfo, BIT_FEATURE_OMPIF) ? 'x' : ' ');
     printf("\n");
-    printf("Managed rstn addr 0x%" PRIX64 "\n", bitinfo_get_managed_rstn_addr(bitinfo));
-    printf("Cmd In addr 0x%" PRIX64 " len %u\n", bitinfo_get_cmd_in_addr(bitinfo), bitinfo_get_cmd_in_len(bitinfo));
-    printf("Cmd Out addr 0x%" PRIX64 " len %u\n", bitinfo_get_cmd_out_addr(bitinfo), bitinfo_get_cmd_out_len(bitinfo));
-    printf("Spawn In ");
-    bitinfo_get_spawn_in_len(bitinfo)
-        ? printf("addr 0x%" PRIX64 " len %u\n", bitinfo_get_spawn_in_addr(bitinfo), bitinfo_get_spawn_in_len(bitinfo))
-        : printf("not enabled\n");
-    printf("Spawn Out ");
-    bitinfo_get_spawn_out_len(bitinfo)
-        ? printf("addr 0x%" PRIX64 " len %u\n", bitinfo_get_spawn_out_addr(bitinfo), bitinfo_get_spawn_out_len(bitinfo))
-        : printf("not enabled\n");
-    printf("Hardware counter ");
-    bitinfo_get_hwcounter_addr(bitinfo) ? printf("addr 0x%" PRIX64 "\n", bitinfo_get_hwcounter_addr(bitinfo))
+    printf("Address map:\n");
+    printf("Managed rstn - address 0x%" PRIX64 "\n", bitinfo_get_managed_rstn_addr(bitinfo));
+    printf("CmdIn - address 0x%" PRIX64 " length %u\n", bitinfo_get_cmd_in_addr(bitinfo),
+           bitinfo_get_cmd_in_len(bitinfo));
+    printf("CmdOut - address 0x%" PRIX64 " length %u\n", bitinfo_get_cmd_out_addr(bitinfo),
+           bitinfo_get_cmd_out_len(bitinfo));
+    printf("SpawnIn - ");
+    bitinfo_get_spawn_in_len(bitinfo) ? printf("address 0x%" PRIX64 " length %u\n", bitinfo_get_spawn_in_addr(bitinfo),
+                                               bitinfo_get_spawn_in_len(bitinfo))
+                                      : printf("not enabled\n");
+    printf("SpawnOut - ");
+    bitinfo_get_spawn_out_len(bitinfo) ? printf("address 0x%" PRIX64 " length %u\n",
+                                                bitinfo_get_spawn_out_addr(bitinfo), bitinfo_get_spawn_out_len(bitinfo))
+                                       : printf("not enabled\n");
+    printf("Hardware counter - ");
+    bitinfo_get_hwcounter_addr(bitinfo) ? printf("address 0x%" PRIX64 "\n", bitinfo_get_hwcounter_addr(bitinfo))
                                         : printf("not enabled\n");
-    printf("POM AXI-Lite ");
-    bitinfo_get_pom_axilite_addr(bitinfo) ? printf("addr 0x%" PRIX64 "\n", bitinfo_get_pom_axilite_addr(bitinfo))
+    printf("POM AXI-Lite - ");
+    bitinfo_get_pom_axilite_addr(bitinfo) ? printf("address 0x%" PRIX64 "\n", bitinfo_get_pom_axilite_addr(bitinfo))
                                           : printf("not enabled\n");
-    printf("Power monitor (CMS) ");
-    bitinfo_get_cms_addr(bitinfo) ? printf("addr 0x%" PRIX64 "\n", bitinfo_get_cms_addr(bitinfo))
+    printf("Power monitor (CMS) - ");
+    bitinfo_get_cms_addr(bitinfo) ? printf("address 0x%" PRIX64 "\n", bitinfo_get_cms_addr(bitinfo))
                                   : printf("not enabled\n");
-    printf("Thermal monitor (sysmon) ");
-    bitinfo_get_sysmon_addr(bitinfo) ? printf("addr 0x%" PRIX64 "\n", bitinfo_get_sysmon_addr(bitinfo))
+    printf("Thermal monitor (sysmon) - ");
+    bitinfo_get_sysmon_addr(bitinfo) ? printf("address 0x%" PRIX64 "\n", bitinfo_get_sysmon_addr(bitinfo))
                                      : printf("not enabled\n");
-    printf("Memory size %" PRIu32 " GB\n", bitinfo_get_memory_size(bitinfo));
 
     int nacc_types = bitinfo_get_acc_type_count(bitinfo);
     bit_acc_type_t* accs = malloc(nacc_types * sizeof(bit_acc_type_t));
